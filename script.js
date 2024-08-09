c9a998b38c163b360fe075219dc8a64d6b5206b5
Ayush Pathak <126178709+ActLikeAyush@users.noreply.github.com>
2024-08-09 17:02:25 +0000

    todayscommit

--- script.js
+++ script.js
@@ -41,6 +41,83 @@ function cmmdd(event) {
                     </tbody>
                 </table>`;
     }
+    else if(first=='skills'){
+
+
+
+        output.innerHTML += `
+        
+    <div class="cont">
+        <div class="skil">
+            <div class="icon">
+                <i id="ht" class="fa-brands fa-html5">  HTML</i>
+                <h1>80%</h1>
+            </div>
+            <div class="ran">
+                <div class="html"></div>
+            </div>
+        </div>
+        <div class="skil">
+            <div class="icon">
+                <i id="css" class="fa-brands fa-css3-alt">  CSS</i>
+                <h1>85%</h1>
+            </div>
+            <div  class="ran">
+                <div  class="css"></div>
+            </div>
+        </div>
+        <div class="skil">
+            <div class="icon">
+                <i id="js" class="fa-brands fa-js">  JavaScript</i>
+                <h1>70%</h1>
+            </div>
+            <div  class="ran">
+                <div class="js"></div>
+            </div>
+        </div>
+        <div class="skil">
+            <div class="icon">
+                <i id="java" class="fa-brands fa-java">  Java</i>
+                <h1>72%</h1>
+            </div>
+            <div  class="ran">
+                <div class="java"></div>
+            </div>
+        </div>
+        <div class="skil">
+            <div class="icon">
+                <i id="sql" class="fa-solid fa-database">  mysql</i>
+                <h1>85%</h1>
+            </div>
+            <div  class="ran">
+                <div class="sql"></div>
+            </div>
+        </div>
+    </div>
+
+        
+        `
+
+
+
+
+
+
+
+
+
+
+    }
+
+    else if(first=='contact'){
+        output.innerHTML=`
+          <div class="mail">
+        <a href="https://www.linkedin.com/in/ayush-pathak-319094241/">Linkedin : Ayush Pathak</a>
+    <a href="https://github.com/ActLikeAyush">GitHub : Ayush Pathak</a>
+    <a href="https://mail.google.com/mail/u/0/?to=ayushpathak1975@gmail.com">Gmail: Ayush Pathak</a>
+    </div>
+        `
+    }
     else if(first==='clear'){
         document.getElementsByClassName('cmdout').value='';
         output.innerHTML='<h1></h1>'
@@ -50,6 +127,7 @@ function cmmdd(event) {
         
         window.location.href="index.html";
     }
+
     else{
         output.innerHTML+=`
         <h2 style=color:red>Invalid Command</h2>
